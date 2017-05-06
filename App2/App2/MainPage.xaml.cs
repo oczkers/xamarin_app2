c28990fe7b3448c2f3ba9a9cec50a59f641b687b
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2017-05-06 21:52:26 +0000

    no more penis, we're polite and show weather

--- App2/App2/MainPage.xaml.cs
+++ App2/App2/MainPage.xaml.cs
@@ -12,31 +12,8 @@ namespace App2
         public MainPage()
         {
             InitializeComponent();
-            device.Text = Device.RuntimePlatform;
-        }
-
-        int count_click = 0;
-
-        private void Button_Clicked(object sender, EventArgs e)
-        {
-            count_click++;
-            txtResult.Text = count_click.ToString();
-            if(count_click<5)
-            {
-                DisplayAlert("", count_click.ToString(), "OK");
-            }
-            else
-            {
-                switch (Device.RuntimePlatform)
-                {
-                    case "Android":
-                        image.Source = "penis.png";
-                        break;
-                    case "Windows":
-                        image.Source = "Assets\\penis.png";
-                        break;
-                }
-            }
+            image.Source = "http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&fdate=2017050612&row=406&col=250&lang=en";
+            // TODO: change url based (only?) on datatime
         }
     }
 }
