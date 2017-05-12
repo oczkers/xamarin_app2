0924003cca24bbc43480138b45266e61476f1fe1
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2017-05-12 23:12:16 +0000

    views.licenses: add clickable links

--- Meteo24/Meteo24/views/licenses.xaml.cs
+++ Meteo24/Meteo24/views/licenses.xaml.cs
@@ -13,6 +13,31 @@ namespace Meteo24.views
         public licenses()
         {
             InitializeComponent();
+
+            var tapGestureMeteo = new TapGestureRecognizer();
+            tapGestureMeteo.Tapped += (s, e) =>
+            {
+                Device.OpenUri(new System.Uri("http://meteo.pl"));
+            };
+            label_meteo_title.GestureRecognizers.Add(tapGestureMeteo);
+            label_meteo.GestureRecognizers.Add(tapGestureMeteo);
+
+            /*
+            var tapGestureGplv3 = new TapGestureRecognizer();
+            tapGestureGplv3.Tapped += (s, e) =>
+            {
+                Device.OpenUri(new System.Uri("http://meteo.pl"));  // TODO: gplv3 on github
+            };
+            label_gplv3_title.GestureRecognizers.Add(tapGestureGplv3);
+            label_gplv3.GestureRecognizers.Add(tapGestureGplv3);
+            */
+
+            var tapGestureGithub = new TapGestureRecognizer();
+            tapGestureGithub.Tapped += (s, e) =>
+            {
+                Device.OpenUri(new System.Uri("https://github.com/oczkers/xamarin_meteo24"));
+            };
+            label_github.GestureRecognizers.Add(tapGestureGithub);
         }
     }
 }
